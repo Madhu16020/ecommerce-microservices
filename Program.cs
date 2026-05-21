09f78b99a8aaca5c52d99118edd8633d5d7fe658
root <root@ip-10-0-1-153.ap-south-1.compute.internal>
2026-05-21 16:43:09 +0000

    Initial ProductService

---
+++ Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/products", () =>
+{
+    return new[]
+    {
+        new { Id = 1, Name = "Laptop" },
+        new { Id = 2, Name = "Phone" }
+    };
+});
+
+app.Run();
